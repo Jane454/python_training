aaeda96c95275cf08c0e3451e9cebf57c65fb511
Jane454 <jane@zwezdin.com>
2019-01-09 13:04:12 +0500

    wd = self.wd поместили внутрь каждого из методов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,29 +1,54 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
+from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 from model.contact import Contact
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
+        self.wd = WebDriver()
         self.wd.implicitly_wait(30)
 
-    def logout(self, wd):
-        # logout
-        wd.find_element_by_link_text("Logout").click()
+    def login(self, username, password):
+        # login
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def return_contact_page(self, wd):
-        # return to contact page
+    def open_home_page(self):
+        # open home page
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def open_contact_page(self):
+        # open contact page
+        wd = self.wd
         wd.find_element_by_link_text("add new").click()
 
-    def submit_contact_creation(self, wd):
+    def logout(self):
+        # logout
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_contact_page(self):
+        # return to contact page
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def submit_contact_creation(self):
+        wd = self.wd
         # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def fill_contact_page(self, wd, contact):
+    def create_contact(self, contact):
+        wd = self.wd
+        self.open_contact_page()
         # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -91,44 +116,13 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
         wd.find_element_by_name("ayear").send_keys(contact.ayear)
-
-    def open_contact_page(self, wd):
-        # open contact page
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, username, password):
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        # open home page
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to.alert()
-        except NoAlertPresentException as e: return False
-        return True
+        self.submit_contact_creation()
+        self.return_contact_page()
 
     def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_contact_page(wd)
-        self.fill_contact_page(wd, Contact(firstname="Pavel", middlename="Petrovich", lastname="Ivanov", nickname="Petro", photo="C:\\Temp\\temp.jpg", title="PP", company="Lenovo", address="Moscow", home = "911", mobile="89020203033", work="02", fax="01", email="petro@gmail.com", email2=" ", email3=" ", homepage=" ", bday="3", bmonth="January", byear="1990", aday="10", amonth="November", ayear="1999" ))
-        self.submit_contact_creation(wd)
-        self.return_contact_page(wd)
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.create_contact(Contact(firstname="Pavel", middlename="Petrovich", lastname="Ivanov", nickname="Petro", photo="C:\\Temp\\temp.jpg", title="PP", company="Lenovo", address="Moscow", home ="911", mobile="89020203033", work="02", fax="01", email="petro@gmail.com", email2=" ", email3=" ", homepage=" ", bday="3", bmonth="January", byear="1990", aday="10", amonth="November", ayear="1999"))
+        self.logout()
 
     def tearDown(self):
         self.wd.quit()
