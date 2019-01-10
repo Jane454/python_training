b53c9d1bee6ac2a4cf3aebe546267cda00c97343
Jane454 <jane@zwezdin.com>
2019-01-11 02:47:54 +0500

    Добавлена валидация фикстуры

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,6 +2,4 @@
 from model.contact import Contact
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Pavel", middlename="Petrovich", lastname="Ivanov", nickname="Petro", photo="C:\\temp\\temp.jpg", title="PP", company="Lenovo", address="Moscow", home ="911", mobile="89020203033", work="02", fax="01", email="petro@gmail.com", email2=" ", email3=" ", homepage=" ", bday="3", bmonth="January", byear="1990", aday="10", amonth="November", ayear="1999"))
-    app.session.logout()
--- test/test_edit_group.py
+++
@@ -1,6 +0,0 @@
-from model.group import Group
-
-def test_edit_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="changed", header="changed", footer="changed"))
-    app.session.logout()
