91f27b6a836b59eede4da0e593808dc1eae8d764
Jane454 <jane@zwezdin.com>
2019-01-11 05:27:26 +0500

    Задание 8. добавлена валидация фикстуры и проверка состояния сессии

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-
 def test_add_group(app):
     app.group.create(Group(name="group1", header="group1", footer="group1"))
-
 
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
