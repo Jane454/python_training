5ec1f95462cb99a5cbe016f0fc83d92fe9579588
Jane454 <jane@zwezdin.com>
2019-01-12 03:53:46 +0500

    Добавлена проверка открыта ли страница группы - если открыта, переход дополнительно не осуществляем.

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,4 @@
+import time
 
 class GroupHelper:
 
@@ -53,8 +54,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        # open group page
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def return_to_groups_page(self):
         # return to group page
