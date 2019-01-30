ae1f5a5ef246e55178588e658fe585ec56573857
Jane454 <jane@zwezdin.com>
2019-01-30 20:52:55 +0500

    Загружаем информацию из тестируемого приложения в виде списков для групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,7 +2,15 @@
 from model.group import Group
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="group1", header="group1", footer="group1"))
+    new_groups = app.group.get_group_list()
+   # убеждаемся что длина нового списка больше на одну запись, True
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    # убеждаемся что длина нового списка больше на одну запись, True
+    assert len(old_groups) + 1 == len(new_groups)
