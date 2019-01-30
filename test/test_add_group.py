8fd47276271998e279b4cc5ad712543f71782805
Jane454 <jane@zwezdin.com>
2019-01-31 00:09:02 +0500

    добавлена сортировка списков перед сравнением для групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,14 +3,19 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="group1", header="group1", footer="group1"))
+    group = (Group(name="group1", header="group1", footer="group1"))
+    app.group.create(group)
     new_groups = app.group.get_group_list()
    # убеждаемся что длина нового списка больше на одну запись, True
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
-    # убеждаемся что длина нового списка больше на одну запись, True
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
