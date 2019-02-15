d866c6646034c9865b371ef44a45c88399e217d0
Jane454 <jane@zwezdin.com>
2019-02-16 00:11:14 +0500

    20_1. добавила для теста модификации группы

--- fixture/group.py
+++ fixture/group.py
@@ -74,6 +74,19 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def modify_group_by_id(self, id, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # open modification form
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        self.fill_group_form(new_group_data)
+        wd.find_element_by_name("update").click()
+        # return to group page
+        self.return_to_groups_page()
+        self.group_cache = None
+
 
     def select_first_group(self):
         wd = self.app.wd
